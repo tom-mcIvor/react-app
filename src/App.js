531cfa900af19e7879c7ed170f3da6668963611d
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-25 01:52:18 +1300

    add stuff

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css'
 
 function App() {
 
-const handleNamechange = () => {
-  const name = ['Bob', 'Kevin', 'Dave']
-  const int
-}
+  const handleNameChange = () =>{
+    const names = ['Bob', 'Kevin', 'Dave' ]
+    const int = Math.floor(Math.random()* 3)
+    return names[int]
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <pre>
+          Text in a pre element is displayed in a fixed-width font, and it
+          preserves both spaces and line breaks
+        </pre>
+        <br />
+
         <p>
-          Hello {name}!
+          Hello {handleNameChange()}!
         </p>
-    
       </header>
     </div>
   )
