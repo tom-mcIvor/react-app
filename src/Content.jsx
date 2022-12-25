94aabdfc482b86f5c1b44c39e00603eed2243512
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-26 00:53:39 +1300

    add

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,13 +1,36 @@
+
+import { useState } from 'react';
+
 const Content = () => {
+
+  const [name, setName] = useState('Dave')
+
   const handleNameChange = () => {
     const names = ['Bob', 'Kevin', 'Dave']
     const int = Math.floor(Math.random() * 3)
-    return names[int]
+    setName(names[int])
+  }
+
+  const handleClick = () => {
+    console.log('You clicked it');
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
   }
 
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>
+        Hello {name}!
+      </p>
+
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={() => handleClick2('Dave')}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
