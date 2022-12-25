324f047791b9d7fe5bb6e78e7703ddd28f2fea85
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-26 02:19:25 +1300

    add

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,35 +3,38 @@ import { useState } from 'react';
 
 const Content = () => {
 
-  const [name, setName] = useState('Dave')
-  const [count, setCount] =useState(0)
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'One half pound bag of Coca Covered Almonds Unsalted'
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Item 2'
+    },
+    {
+      id: 3,
+      checked: false,
+      items: 'Item 3'
+    }
+  ])
 
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave']
-    const int = Math.floor(Math.random() * 3)
-    setName(names[int])
-  }
-
-  const handleClick = () => {
-    setCount(count + 1)
-    setCount(count + 1)
-    console.log(count);
-  }
-
-  const handleClick2 = () => {
-    console.log(count);
-  }
-
-  
   return (
     <main>
-      <p onDoubleClick={handleClick}>
-        Hello {name}!
-      </p>
-
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleClick}>Click it</button>
-      <button onClick={handleClick2}>Click it</button>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <button>Delete</button>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
