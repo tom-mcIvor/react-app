288ab5642844e7c38d489e3f2ac7a3449c5330db
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2022-12-28 02:28:29 +1300

    add

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,40 +1,21 @@
 
-import { useState } from 'react';
+import ItemList from "./ItemList";
 
-const Content = () => {
+const Content = ({ items, handleCheck, handleDelete }) => {
 
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'One half pound bag of Coca Covered Almonds Unsalted'
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'Item 2'
-    },
-    {
-      id: 3,
-      checked: false,
-      items: 'Item 3'
-    }
-  ])
 
   return (
+
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className='item' key={item.id}>
-            <input
-              type="checkbox"
-              checked={item.checked}
-            />
-            <label>{item.item}</label>
-            <button>Delete</button>
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )}
     </main>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ title }) => {
 
-  
+
   return (
-  <header >
-    <h1>Groceries List</h1>
-  </header>
+    <header >
+      <h1>{title}</h1>
+    </header>
 
-)
+  )
+}
+
+Header.defaultProps = {
+  title: 'Default Title'
 }
 
 export default Header
